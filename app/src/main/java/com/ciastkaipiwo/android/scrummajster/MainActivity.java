f68902e5545461b9292f0f64563cf70c5275e9df
Daniel Kobylski <daniel.kobylski@smcebi.edu.pl>
2018-03-31 13:17:30 +0200

    Initial commit

---
+++ app/src/main/java/com/ciastkaipiwo/android/scrummajster/MainActivity.java
@@ -0,0 +1,13 @@
+package com.ciastkaipiwo.android.scrummajster;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
